30d26fa3cbb879522d845185a6a6ab720c707820
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-07-24 15:56:29 +0100

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewBornComponent } from './pages/new-born/new-born.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BabyComponent } from './pages/baby/baby.component';
+import { ChildrenComponent } from './pages/children/children.component';
+import { JuniorComponent } from './pages/junior/junior.component';
 
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "primera-puesta", component:NewBornComponent},
+  {path: "bebe", component: BabyComponent},
+  {path: "infantil", component: ChildrenComponent},
+  {path:"junior", component: JuniorComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BabyComponent } from './pages/baby/baby.component';
+import { NewBornComponent } from './pages/new-born/new-born.component';
+import { ChildrenComponent } from './pages/children/children.component';
+import { JuniorComponent } from './pages/junior/junior.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent ,
+    BabyComponent,
+    NewBornComponent,
+    ChildrenComponent,
+    JuniorComponent
   ],
   imports: [
     BrowserModule,
