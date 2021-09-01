338f18634047db4d418bbc03440a95ea429dcd7c
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-09-01 19:31:01 +0100

    Product route added with match pattern

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule , UrlSegment } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
-import {categoryRoutes} from './models/category';
+import { categoryRoutes } from './models/category';
+import { ProductComponent } from './pages/product/product.component';
 
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "home", component: HomeComponent},
+  {matcher: (url : UrlSegment[]) => {
+    return url.length === 1 && url[0].path.match(/\w+.html/g)
+    ? { consumed: url } 
+    :  null;
+  },component: ProductComponent}
 ];
 categoryRoutes.forEach(element => {
   routes.push(element);
